f4531dda06cbcae659741618162de77363392286
afafa82 <73514070+afafa82@users.noreply.github.com>
2021-10-06 06:45:12 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+try {
+  fs.mkdirSync("Logs");
+  console.log("Logs directory created");
+  let files = [];
+
+  for (let i = 0; i < 10; i++) {
+    const fileName = `log${i}.txt`;
+    fs.writeFileSync("./Logs/" + fileName, "This is log" + i);
+    console.log(fileName);
+    files.push(fileName);
+  }
+  for (const file of files) {
+    fs.unlinkSync("./Logs/" + file);
+    console.log("delete files..." + file);
+  }
+
+  // fs.rmdirSync("Logs", { recursive: true });
+} catch (e) {
+  if (e.code == "exist") {
+    console.log("The director named Logs exists");
+  } else {
+    console.log(e);
+  }
+}
